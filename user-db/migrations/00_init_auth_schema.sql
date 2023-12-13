916a8ae88317df199ea9b49aa6459c4e17d3df2d
BragdonD <thomasduclos1012@gmail.com>
2023-12-13 09:45:10 +0100

    feat(user-db): create function to retrieve user role and id from request cookie

--- user-db/migrations/00_init_auth_schema.sql
+++ user-db/migrations/00_init_auth_schema.sql
@@ -2,21 +2,29 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-create table users (
+CREATE TYPE if not exists user_role AS ENUM ('anon', 'admin', 'user', 'moderator');
+
+create table if not exists users (
     user_id uuid primary key default uuid_generate_v4(),
     email text unique,
     phone text unique,
     password text not null,
     verified boolean default false,
+    role user_role default 'user',
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
     check (email is not null or phone is not null)
 );
+comment on table users is 'Auth: Stores user credentials';
 
-alter table users enable row level security;
+-- get the user id from the request cookie
+create or replace function auth.uid() returns uuid as $$
+    select nullif(current_setting('request.jwt.claim.sub', true), '')::uuid;
+$$ language sql stable;
 
--- create policy "Allow logged-in users to see their own data"
--- on users 
--- for select 
+--get the user role from the request cookie
+create or replace function auth.role() returns text as $$
+    select nullif(current_setting('request.jwt.claim.role', true), '')::text;
+$$ language sql stable;
 
 COMMIT;
