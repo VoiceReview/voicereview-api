c5933b63413e495c2a3e9687968b0c3035019342
BragdonD <thomasduclos1012@gmail.com>
2024-01-06 12:04:11 +0100

    fix(user-db): make db wait to be healthy before applying migration

--- user-db/reset-database.sh
+++ user-db/reset-database.sh
@@ -3,5 +3,14 @@
 docker compose down
 docker volume rm api_data    
 docker compose up -d
+
+# Need to wait for the database to start
+sleep 10
+
+# Check if the user_database service is healthy
+while [[ "$(docker ps --filter "name=user_database" --format "{{.Status}}")" != *"healthy"* ]]; do
+    sleep 1
+done
+
 chmod a+x ./apply-migrations.sh
 ./apply-migrations.sh
