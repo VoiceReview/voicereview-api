fccbe647ec37da94c2aa4109d3a096362243b5c5
BragdonD <thomasduclos1012@gmail.com>
2024-01-06 16:46:21 +0100

    feat(user-db): change session table to access_token

--- user-db/migrations/00_init_auth_schema.sql
+++ user-db/migrations/00_init_auth_schema.sql
@@ -15,15 +15,15 @@ create table if not exists users (
 );
 comment on table users is 'Auth: Stores user credentials';
 
-create table if not exists sessions (
-    session_id uuid primary key default uuid_generate_v4(),
+create table if not exists access_tokens (
+    token uuid primary key default uuid_generate_v4(),
     user_id uuid references users(user_id) on delete cascade,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
-    expires_at timestamp default current_timestamp + interval '1 day',
+    expires_at timestamp default current_timestamp + interval '15 minutes',
     revoked boolean default false
 );
-comment on table sessions is 'Auth: Stores session tokens';
+comment on table access_tokens is 'Auth: Stores access tokens';
 
 create table if not exists refresh_tokens (
     token uuid primary key default uuid_generate_v4(),
@@ -34,6 +34,23 @@ create table if not exists refresh_tokens (
     revoked boolean default false
 );
 comment on table refresh_tokens is 'Auth: Stores refresh tokens';
+
+-- create cron job to delete expired tokens
+create or replace function delete_expired_tokens() returns trigger as $$
+begin
+    delete from access_tokens where expires_at < current_timestamp;
+    delete from refresh_tokens where expires_at < current_timestamp;
+    return null;
+end;
+$$ language plpgsql;
+
+create trigger delete_expired_tokens
+    after insert on access_tokens
+    execute procedure delete_expired_tokens();
+
+create trigger delete_expired_tokens
+    after insert on refresh_tokens
+    execute procedure delete_expired_tokens();
 
 -- get the user id from the request cookie
 create or replace function uid() returns uuid as $$
