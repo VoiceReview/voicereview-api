efa568cfbd2ff4c80121907ced673b0e60539ab9
BragdonD <thomasduclos1012@gmail.com>
2024-01-06 12:03:12 +0100

    fix(user-db): remove role type

--- user-db/migrations/00_init_auth_schema.sql
+++ user-db/migrations/00_init_auth_schema.sql
@@ -2,15 +2,13 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TYPE if not exists user_role AS ENUM ('anon', 'admin', 'user', 'moderator');
-
 create table if not exists users (
     user_id uuid primary key default uuid_generate_v4(),
     email text unique,
     phone text unique,
     password text not null,
     verified boolean default false,
-    role user_role default 'user',
+    role text default 'user',
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
     check (email is not null or phone is not null)
@@ -22,6 +20,7 @@ create table if not exists sessions (
     user_id uuid references users(user_id) on delete cascade,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
+    expires_at timestamp default current_timestamp + interval '1 day',
     revoked boolean default false
 );
 comment on table sessions is 'Auth: Stores session tokens';
@@ -31,19 +30,18 @@ create table if not exists refresh_tokens (
     user_id uuid references users(user_id) on delete cascade,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
+    expires_at timestamp default current_timestamp + interval '30 days',
     revoked boolean default false
 );
 comment on table refresh_tokens is 'Auth: Stores refresh tokens';
 
-
-
 -- get the user id from the request cookie
-create or replace function auth.uid() returns uuid as $$
+create or replace function uid() returns uuid as $$
     select nullif(current_setting('request.jwt.claim.sub', true), '')::uuid;
 $$ language sql stable;
 
 --get the user role from the request cookie
-create or replace function auth.role() returns text as $$
+create or replace function role() returns text as $$
     select nullif(current_setting('request.jwt.claim.role', true), '')::text;
 $$ language sql stable;
 
