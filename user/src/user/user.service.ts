c5b0e7a64e954b7881256787e63523c388c02186
BragdonD <thomasduclos1012@gmail.com>
2024-01-06 12:05:50 +0100

    feat(user): user can be registered

--- user/src/user/user.service.ts
+++ user/src/user/user.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { user } from './interfaces/user.interface';
+import { DatabaseService } from 'src/database/database.service';
+import { QueryResult } from 'pg';
+import { Users } from 'src/database/database.types';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        private readonly databaseService: DatabaseService
+    ) { }
+
+    findOne(uuid: string) {
+        return {
+            uuid: 'uuid',
+            email: 'email',
+            phone: 'phone',
+            password: 'password',
+            verified: true,
+            role: 'ADMIN',
+            createdAt: 'createdAt',
+            updatedAt: 'updatedAt',
+        }
+    }
+
+    findAll() {
+        return [
+            {
+                uuid: 'uuid',
+                email: 'email',
+                phone: 'phone',
+                password: 'password',
+                verified: true,
+                role: 'ADMIN',
+                createdAt: 'createdAt',
+                updatedAt: 'updatedAt',
+            }
+        ]
+    }
+
+    createOne({ email, phone, password, role }: { email: string, phone: string, password: string, role: user.UserRole }) : Promise<QueryResult<Users>>  {
+        return this.databaseService.query(
+            'INSERT INTO users (email, phone, password, role) VALUES ($1, $2, $3, $4) RETURNING *',
+            [email, phone, password, role]
+        );
+    }
+}
