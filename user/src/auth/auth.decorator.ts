15de7f16c2931827e48f9a72ef1f6b8121f80332
BragdonD <thomasduclos1012@gmail.com>
2024-02-12 11:16:58 +0100

    fix: remove old user service

--- user/src/auth/auth.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const Auth = (...args: string[]) => SetMetadata('isAuth', {
-    isAuth: true,
-    args
-});
--- user/src/auth/dto/RefreshRequest.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class RefreshRequest {
-    @IsNotEmpty()
-    @IsString()
-    refreshToken: string;
-}
